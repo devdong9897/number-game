5bfd367a3ce5c3cd20af83aec515cfe33433866e
devdong9897 <dev.dong9897@gmail.com>
2024-11-24 16:31:57 +0900

    유저번호와 랜덤번호 비교

--- main.js
+++ main.js
@@ -7,4 +7,39 @@
 // 유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깍지 않는다.
 
+let computer = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("userInput");
+let resultArea = document.getElementById("result-area");
 
+playButton.addEventListener("click", play);
+
+function random() {
+  computer = Math.floor(Math.random() * 100) + 1;
+  console.log(computer);
+}
+
+function play() {
+  const userValue = userInput.value;
+
+  let imgElement = document.createElement("img");
+  imgElement.style.width = "300px";
+  imgElement.style.height = "200px";
+
+  if (userValue < computer) {
+    imgElement.src = "images/up.gif";
+    resultArea.innerHTML = "";
+    resultArea.appendChild(imgElement);
+  } else if (userValue > computer) {
+    imgElement.src = "images/down.gif";
+    resultArea.innerHTML = "";
+    resultArea.appendChild(imgElement);
+  } else {
+    imgElement.src = "images/answer.gif";
+    resultArea.innerHTML = "";
+    resultArea.appendChild(imgElement);
+    resultArea.textContent = "맞췄습니다!!!";
+  }
+}
+
+random();
